f4d9702dd67c88cd402d507024bd28f41e4419a0
Mohamed Ali Louati <140558057+dali-lt@users.noreply.github.com>
2025-10-01 12:07:52 +0100

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,8 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react-swc'
 
+// https://vitejs.dev/config/
 export default defineConfig({
-  base: '/firelytics/',
-  plugins: [react()]
+  plugins: [react()],
+  base: '/firelytics/',   // اسم الـ repo متاعك
 })
